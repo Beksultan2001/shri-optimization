5d671b6ffeacb9b78bc33a7b22a67b81edeca651
b.omirzak <b.omirzak@documentolog.com>
2023-07-07 20:33:41 +0600

    3000 fix

--- webpack.config.js
+++ webpack.config.js
@@ -12,9 +12,10 @@ const stylesHandler = 'style-loader';
 const config = {
     entry: './src/main.jsx',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'vendors'),
     },
-    plugins: [],
+    plugins: [
+    ],
     mode: 'production',
     optimization: {
         usedExports: true,
@@ -33,6 +34,9 @@ const config = {
                 test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
                 type: 'asset',
             },
+
+            // Add your rules for custom modules here
+            // Learn more about loaders from https://webpack.js.org/loaders/
         ],
     },
 };
